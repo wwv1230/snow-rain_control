04c425688de0ea2d4410a868eb1a8c8ad1c11910
wwv1230 <73861551+wwv1230@users.noreply.github.com>
2020-12-01 11:49:39 +0900

    슬라이더를 이용한 원의 모양 바꾸기/기존 코드 삭제

--- sketch.js
+++ sketch.js
@@ -1,74 +1,15 @@
-class Particle {
-  constructor(position) {
-    this.acceleration = createVector(0, 0.0);
-    this.velocity = createVector(random(-1, 1), random(-2, 0));
-    this.position = position.copy();
-    this.lifespan = 255.0;
-    this.mass = 1; // Let's do something better here!
-  }
-
-  run() {
-    this.update();
-    this.display();
-  }
-
-  applyForce(force) {
-    let f = force.copy();
-    f.div(this.mass);
-    this.acceleration.add(f);
-  }
-
-  // Method to update position
-  update() {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.acceleration.mult(0);
-    this.lifespan -= 2.0;
-  }
-
-  // Method to display
-  display() {
-    stroke(200, this.lifespan);
-    strokeWeight(2);
-    fill(127, this.lifespan);
-    ellipse(this.position.x, this.position.y, 12, 12);
-  }
-
-  // Is the particle still useful?
-  isDead() {
-    if (this.lifespan < 0.0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+let slider;
+function setup() {
+  createCanvas(400,400);
+  slider = createSlider(0, 30, 100);
+  slider.position(10, 10);
+  slider.style('width', '150px');
 }
 
-class ParticleSystem {
-  constructor(position) {
-    this.origin = position.copy();
-    this.particles = [];
-  }
+function draw() {
+  background(200);
+  let val = slider.value();
+  stroke(0,0,255);
+  ellipse(width/2, height/2, val, 30);
 
-  addParticle() {
-    this.particles.push(new Particle(this.origin));
-  }
-
-  run() {
-    // Run every particle
-    // ES6 for..of loop
-    for (let particle of this.particles) {
-      particle.run();
-    }
-
-    // Filter removes any elements of the array that do not pass the test
-    this.particles = this.particles.filter(particle => !particle.isDead());
-  }
-
-  // A function to apply a force to all Particles
-  applyForce(f) {
-    for (let i = 0; i < this.particles.length; i++) {
-      this.particles[i].applyForce(f);
-    }
-  }
 }
