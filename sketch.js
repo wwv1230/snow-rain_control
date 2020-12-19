d03766d9aa5d456798ec987709af79cd7179c2cb
wwv1230 <73861551+wwv1230@users.noreply.github.com>
2020-12-19 20:41:49 +0900

    배열을 추가하고, Particle class를 추가하였다

--- sketch.js
+++ sketch.js
@@ -1,19 +1,34 @@
+let particle = [] ;
 let slider;
 let force = 0;
 
 function setup() {
-  createCanvas(1080,720);
+  createCanvas(400,400);
+    for (let i = 0; i < 20; i++) {
+    particle.push(new Particle());
+  }
   slider = createSlider(0, 30, 100);
   slider.position(10, 10);
   slider.style('width', '150px');
 }
 
 function draw() {
-
-  force = force + 2 ;
   background(140,190,255);
-  let val = slider.value();
-  stroke(0,0,255);
-  ellipse(width/2, force, val, 30);
+  for (let i = 0; i < particle.length; i++){
+  particle[i].snow_rain();
+  }
 
+}
+
+class Particle {
+  snow_rain() {
+    force = force + random(0.1);
+    let val = slider.value();
+    stroke(0,0,255);
+    ellipse(random(width), force, val, 30);
+
+    if (force > height) {
+      force = 0;
+    }
+  }
 }
